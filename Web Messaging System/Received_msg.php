d6075469c130ee3fb55cdcb0c74dcbfbb2feaee7
MD ASIF TALUKDAR <asifmason123@gmail.com>
2021-02-10 04:09:09 +0600

    file modified using file from htdocs

--- Web Messaging System/Received_msg.php
+++ Web Messaging System/Received_msg.php
@@ -8,34 +8,34 @@
 <title>Received Messages</title>
 <link rel="stylesheet" href="css/style.css">
 <?php
-   $id=$_SESSION['id'];
+   $id=$_SESSION['userid'];
    echo $id;
   ?>
 </head>
 <body  background="maxresdefault.jpg" >
-  
+
   <div id="main-wrapper">
   <center>
   <?php
-  
+
   $query="select * from msg WHERE Recipient_id=$id ";
 
 
 
-				
+
 				    $query_run = mysqli_query($con,$query);
     if(mysqli_num_rows($query_run)>0)
-	{   
+	{
 
       while($row = mysqli_fetch_array($query_run,MYSQLI_ASSOC))
 	  {
-   
-echo "<br><br>  Message:   ";   
+
+echo "<br><br>  Message:   ";
 echo "<font size='4' color='red'>{$row['Message']}</font>";
 echo "<br>  Sent by:  ";
   $id=$row['User_id'] ;
 
-$quer="select * from user WHERE id=$id";
+$quer="select * from userinfo WHERE id=$id";
 $query_ru = mysqli_query($con,$quer);
 $row= mysqli_fetch_array($query_ru,MYSQLI_ASSOC);
 echo $row['username'];
@@ -44,15 +44,15 @@ echo "<br>";
 	}
 	 else{
 				    echo "<br><b><font size='8' color='blue'>No messages<br></b>";
-				   
+
 			    }
-	
+
   ?>
 
     <a href="homepage.php"><input type="button" id="back_btn" value="Back"/></a>
   </center>
-  
-  
+
+
   </div>
 </body>
 </html>
--- Web Messaging System/index.php
+++ Web Messaging System/index.php
@@ -9,13 +9,13 @@
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body  background="maxresdefault.jpg" >
-  
+
   <div id="main-wrapper">
   <center><h2>login Form</h2>
   <img src="imgs/avatar.png" class="avatar"/>
   </center>
-  
-  
+
+
   <form class="myform" action="index.php" method="post">
   <label><br><b>Id:</b></br></label>
   <input name="id" type="text" class="inputvalues" placeholder="Type your id" />
@@ -24,34 +24,34 @@
   <input name="login" type="submit" id="login_btn" value="Login"/></br>
   <a href="register.php"><input type="button" id="register_btn" value="Register"/></a>
   </form>
-  
+
   <?php
   if(isset($_POST['login']))
   {
 	$id=$_POST['id'];
     $password=$_POST['password'];
-	
 
-    $query="select * from user WHERE id='$id' AND password='$password'";
+
+    $query="select * from userinfo WHERE id='$id' AND password='$password'";
 
     $query_run = mysqli_query($con,$query);
     if(mysqli_num_rows($query_run)>0)
 	{
 		$_SESSION['id']=$id;
 		echo("<script>window.location.href = 'homepage.php' </script>");
-		
-	}	
+
+	}
 	else
 	{
-		
+
 		echo '<script type="text/javascript"> alert("Invalid credentials")</script>';
 	}
-	  
+
   }
-  
+
   ?>
-  
-  
+
+
   </div>
 </body>
 </html>
